2c8ce90b035fb55b069d589109a6ec1ba2734675
muluante <muluante@outlook.com>
2023-11-02 09:43:58 +0000

    3-app

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -1,34 +1,41 @@
 #!/usr/bin/env python3
-"""Flask babel babel setup"""
-from flask import Flask, render_template, request
-from flask_babel import Babel, gettext as _
-
+""" Parametrize templates
+"""
+from flask import (
+    Flask,
+    render_template,
+    request
+)
+from flask_babel import Babel
 
 app = Flask(__name__)
 babel = Babel(app)
 
 
 class Config:
-    """Configure available languages in our app"""
-    LANGUAGES = ["en", "fr"]
-    BABEL_DEFAULT_LOCALE = "en"
-    BABEL_DEFAULT_TIMEZONE = "UTC"
+    """ Config
+    """
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
 
 
-app.config.from_object('3-app.Config')
+app.config.from_object(Config)
 
 
 @babel.localeselector
-def get_locale() -> str:
-    """Get locale from request"""
-    return request.accept_languages.best_match('3-app.Config.LANGUAGES')
+def get_locale():
+    """ determine the best match with our supported languages
+    """
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
-@app.route("/")
-def home() -> str:
-    """return 1-index.html"""
-    return render_template("3-index.html")
+@app.route('/')
+def index():
+    """ return a hello world page
+    """
+    return render_template('3-index.html')
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True, host='0.0.0.0', port=5000)
