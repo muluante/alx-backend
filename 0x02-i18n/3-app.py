8866bbe12c33e48e3401df6fcb7e751fa63d9e8a
muluante <muluante@outlook.com>
2023-11-02 09:53:25 +0000

    3-app

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -1,34 +1,46 @@
 #!/usr/bin/env python3
-""" Module for trying out Babel i18n """
-from flask_babel import Babel, _
-from flask import Flask, render_template, request, flash
+"""A simple flask app
+"""
 
-app = Flask(__name__, template_folder='templates')
-babel = Babel(app)
+
+from flask import Flask, render_template, request
+from flask_babel import Babel
 
 
 class Config(object):
-    """ Configuration Class for Babel """
+    """_summary_
 
+    Returns:
+            _type_: _description_
+    """
     LANGUAGES = ['en', 'fr']
     BABEL_DEFAULT_LOCALE = 'en'
     BABEL_DEFAULT_TIMEZONE = 'UTC'
 
 
+# configure the flask app
+app = Flask(__name__)
 app.config.from_object(Config)
-
-
-@app.route('/', methods=['GET'], strict_slashes=False)
-def hello_world() -> str:
-    """Renders a Basic Template for Babel Implementation"""
-    return render_template("3-index.html")
+app.url_map.strict_slashes = False
+babel = Babel(app)
 
 
 @babel.localeselector
-def get_locale() -> str:
-    """Select a language translation to use for that request"""
+def get_locale():
+    """_summary_
+
+    Returns:
+            _type_: _description_
+    """
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
-if __name__ == "__main__":
-    app.run()
+@app.route('/')
+def index():
+    """_summary_
+    """
+    return render_template('3-index.html')
+
+
+if __name__ == '__main__':
+    app.run(port="5000", host="0.0.0.0", debug=True)
