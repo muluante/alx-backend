3ed14c5eba59af5c3f06bb838798e0a61a40824c
muluante <muluante@outlook.com>
2023-11-02 09:39:33 +0000

    3-app

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -1,37 +1,34 @@
 #!/usr/bin/env python3
-"""A Basic Flask app with internationalization support.
-"""
-from flask_babel import Babel
+"""Flask babel babel setup"""
 from flask import Flask, render_template, request
+from flask_babel import Babel, gettext as _
+
+
+app = Flask(__name__)
+babel = Babel(app)
 
 
 class Config:
-    """Represents a Flask Babel configuration.
-    """
+    """Configure available languages in our app"""
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
 
 
-app = Flask(__name__)
-app.config.from_object(Config)
-app.url_map.strict_slashes = False
-babel = Babel(app)
+app.config.from_object('3-app.Config')
 
 
 @babel.localeselector
 def get_locale() -> str:
-    """Retrieves the locale for a web page.
-    """
-    return request.accept_languages.best_match(app.config["LANGUAGES"])
+    """Get locale from request"""
+    return request.accept_languages.best_match('3-app.Config.LANGUAGES')
 
 
-@app.route('/')
-def get_index() -> str:
-    """The home/index page.
-    """
-    return render_template('3-index.html')
+@app.route("/")
+def home() -> str:
+    """return 1-index.html"""
+    return render_template("3-index.html")
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run()
